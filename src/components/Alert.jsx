867d01b31ef66e73406dfbd9713720846acc1a25
michelle perez <mmorales@boomsourcing.com>
2020-02-04 14:41:57 -0700

    file for github deployment

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,18 +1,22 @@
-import React, { useContext} from 'react'
-import AlertContext from '../context/alert/alertContext'
+import React, { useContext } from "react"
+import AlertContext from "../context/alert/alertContext"
 
 const Alert = () => {
-    const alertContext = useContext(AlertContext)
+  const alertContext = useContext(AlertContext)
 
-    const {alert} = alertContext
+  const { alert } = alertContext
 
-    return (
-        alert !== null &&
-            <div class="alert alert-danger" role="alert" style={{position: 'fixed', zIndex: 999, top: 15, right: 21}}>
-                {alert.msg}
-            </div>
-        
+  return (
+    alert !== null && (
+      <div
+        className="alert alert-danger"
+        role="alert"
+        style={{ position: "fixed", zIndex: 999, top: 15, right: 21 }}
+      >
+        {alert.msg}
+      </div>
     )
+  )
 }
 
 export default Alert
--- src/views/finder/Finder.jsx
+++ src/views/finder/Finder.jsx
@@ -11,7 +11,7 @@ const Finder = () => {
 
   useEffect(() => {
     githubContext.getUsers()
-    alertContext.setAlert("Welcome to Github search")
+    //alertContext.setAlert("Welcome to Github search")
   }, [])
 
   return (
